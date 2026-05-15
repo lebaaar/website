ccbdfac58c04234cef4bc5c5d3c3cbd6831810f2
Lan <lanlebar6@gmail.com>
2026-05-15 17:21:05 +0200

    styling fixes

--- src/lib/components/About.svelte
+++ src/lib/components/About.svelte
@@ -64,9 +64,9 @@
 						{#each experiences as exp, index (index)}
 							<div class="relative flex gap-4 pb-10 last:pb-0">
 								{#if index <= experiences.length || true}
-									<div class="absolute left-[9px] top-7 h-[calc(100%-0.25rem)] w-px bg-gradient-to-b from-indigo-400/70 to-transparent"></div>
+									<div class="absolute left-2.25 top-7 h-[calc(100%-0.25rem)] w-px bg-linear-to-b from-indigo-400/70 to-transparent"></div>
 								{/if}
-								<div class="relative mt-1 h-5 w-5 shrink-0 rounded-full bg-gradient-to-br from-purple-500 to-blue-500"></div>
+								<div class="relative mt-1 h-5 w-5 shrink-0 rounded-full bg-linear-to-br from-purple-500 to-blue-500"></div>
 								<div class="flex-1">
 									<div class="mb-2 flex flex-wrap items-baseline justify-between gap-2">
 										<h4 class="text-xl font-semibold text-white">{exp.role}</h4>
