8acdd8ebb6d0a85aaeea25851b4f7040f86ea96e
Lan <lanlebar6@gmail.com>
2026-05-17 23:17:47 +0200

    disallow clankers on /cenko

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,7 +3,11 @@ import type { Handle } from '@sveltejs/kit';
 
 export const handle: Handle = ({ event, resolve }) =>
 	// eslint-disable-next-line @typescript-eslint/no-unused-vars
-	paraglideMiddleware(event.request, ({ request, locale: _locale }) => {
+	paraglideMiddleware(event.request, async ({ request, locale: _locale }) => {
 		event.request = request;
-		return resolve(event);
+		const response = await resolve(event);
+		if (event.url.pathname.startsWith('/cenko')) {
+			response.headers.set('X-Robots-Tag', 'noindex, nofollow, noarchive, nositelinkssearchbox');
+		}
+		return response;
 	});
