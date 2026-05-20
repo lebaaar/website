13038fa9fe981b94e60a5c4ac6fb9e1961ae6195
Lan <lanlebar6@gmail.com>
2026-05-20 18:42:01 +0200

    remove cenko stuff

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,11 +3,7 @@ import type { Handle } from '@sveltejs/kit';
 
 export const handle: Handle = ({ event, resolve }) =>
 	// eslint-disable-next-line @typescript-eslint/no-unused-vars
-	paraglideMiddleware(event.request, async ({ request, locale: _locale }) => {
+	paraglideMiddleware(event.request, ({ request, locale: _locale }) => {
 		event.request = request;
-		const response = await resolve(event);
-		if (event.url.pathname.startsWith('/cenko')) {
-			response.headers.set('X-Robots-Tag', 'noindex, nofollow, noarchive, nositelinkssearchbox');
-		}
-		return response;
+		return resolve(event);
 	});
--- src/routes/cenko/+layout.svelte
+++
@@ -1,6 +0,0 @@
-<svelte:head>
-	<meta name="robots" content="noindex, nofollow, noarchive, nositelinkssearchbox" />
-	<link rel="icon" href="/cenko.ico" sizes="any" />
-</svelte:head>
-
-<slot />
