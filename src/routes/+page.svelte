4228222f1109f43ea657e2f775b10f29cabe3118
Lan <lanlebar6@gmail.com>
2026-07-30 17:57:17 +0200

    update email

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -118,7 +118,7 @@
 					</svg>
 					LinkedIn
 				</a>
-				<a use:shine href="mailto:lebar@lan.si" target="_blank" rel="noopener noreferrer" class={socialLinkClass}>
+				<a use:shine href="mailto:hello@lan.si" target="_blank" rel="noopener noreferrer" class={socialLinkClass}>
 					<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
 						<rect width="20" height="16" x="2" y="4" rx="2"/><path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"/>
 					</svg>
